6f769a35381020d43086110329bae751975f4f79
Keyur <keyurvaitha21@gmail.com>
2025-02-07 06:45:54 +0530

    WhoToFollow added, button pending

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  
+  const user= await currentUser();
   return (
   <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
     <div className="lg:col-span-6">
         <CreatePost/>
     </div>
     <div className="hidden lg:block lg:col-span-4 sticky top-20">
-         Who To Follow 
+         <WhoToFollow/>
     </div>
    
   </div>
