da3b7e32bf0c9240a82bf1fd1e1b20d9fadca1be
Keyur <keyurvaitha21@gmail.com>
2025-02-07 06:13:13 +0530

    Added post but  some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button"
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  
   return (
-  <h1>Home Page Content</h1>
+  <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+    <div className="lg:col-span-6">
+        <CreatePost/>
+    </div>
+    <div className="hidden lg:block lg:col-span-4 sticky top-20">
+         Who To Follow 
+    </div>
+   
+  </div>
  );
 }
