df7975194f6a78f3143d82b5fc18a5497b3996f8
Keyur <keyurvaitha21@gmail.com>
2025-02-08 00:15:57 +0530

    Added profile section

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,7 +1,7 @@
 import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { SignUpButton, UserButton } from "@clerk/nextjs";
 import ModeToggle from "./ModeToggle";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -40,9 +40,9 @@ async function DesktopNavbar() {
           <UserButton />
         </>
       ) : (
-        <SignInButton mode="modal">
-          <Button variant="default">Sign In</Button>
-        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant="default">Sign Up</Button>
+        </SignUpButton>
       )}
     </div>
   );
